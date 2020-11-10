561aebb1ab18f4a7f0cf726590b12d0993af4465
gstorozhuk <galyna.storozhuk@gmail.com>
2020-11-10 12:49:55 -0500

    project is already in remote repo, committing more changes

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
         System.out.println("Hello");
 
         // B21 is good in IntelliJ
+
+        // I have already published on GitHub.com
+        // now I am ready to make more commits
+        // Do O need to share again?
+        // Absolutely NO;
+        // Commit and just PUSH
+
     }
 }
